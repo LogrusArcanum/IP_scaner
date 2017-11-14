ebeea18552fe86a7b22a35c7902a5d541a5f5c91
LogrusArcanum <mario0mikecc@gmail.com>
2017-11-14 01:07:54 +0100

    Reformat

--- NetworkDiscoveryCore.py
+++ NetworkDiscoveryCore.py
@@ -3,6 +3,7 @@ import csv
 import os
 
 class NetworkDiscoveryCore:
+
     def __init__(self):  # Open file for read/write when class define
         self.file_path = '/home/mile/Documents/ALL_IP.csv'
         if os.path.exists(self.file_path):  # Open file or create
@@ -24,19 +25,19 @@ class NetworkDiscoveryCore:
         self.subnet = input('Input subnet: ') or '192.168.1.'
         print('Using default: ', self.subnet)
         try:
-            self.start = int(input('Input start ip: '))
-            self.stop = int(input('Input stop ip: '))
+            self.start_ip = int(input('Input start_ip ip: '))
+            self.stop_ip = int(input('Input stop_ip ip: '))
         except ValueError:
-            self.start = 1
-            self.stop = 256
+            self.start_ip = 1
+            self.stop_ip = 256
 
     def close_file(self):  # Close file
         self.wr_file.close()
 
     def perform_net_discovery(self):  # Define network discovery
         self.input_ip()
-        print(self.start, self.stop)
-        for i in range(self.start, self.stop):
+        print(self.start_ip, self.stop_ip)
+        for i in range(self.start_ip, self.stop_ip):
             ip = self.subnet + str(i)
             try:
                 ip1 = socket.gethostbyaddr(ip)
@@ -66,31 +67,3 @@ class NetworkDiscoveryCore:
         ip1 = socket.gethostbyname(socket.gethostname())
         hostname = socket.getfqdn()
         print(hostname, '', ip1)
-
-
-class NetworkDiscoveryMenu(NetworkDiscoveryCore):
-    def main_menu(self):
-        print('''
-        1.  Open File
-        2.  Network Discovery
-        3.  Read csv File
-        ''')
-
-    def main_menu_choice(self):
-        exit_menu = True
-        choice_input = input("> ")
-        while exit_menu:
-            if choice_input == '1':
-                self.input_path()
-            elif choice_input == '2':
-                self.perform_net_discovery()
-            elif choice_input == '3':
-                self.read_csv_file()
-            elif choice_input == '0':
-                exit_menu = False
-                break
-            elif choice_input == 'm':
-                self.main_menu()
-            else:
-                print('Error')
-            choice_input = input("> ")
