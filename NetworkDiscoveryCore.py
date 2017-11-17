14ff461b4c79ba50dfa67ae9173e642501776b6a
Mario <crveni0mrav@gmail.com>
2017-11-17 19:44:12 +0100

    Update NetworkDiscoveryCore.py

--- NetworkDiscoveryCore.py
+++ NetworkDiscoveryCore.py
@@ -4,12 +4,19 @@ import os
 
 
 class NetworkDiscoveryCore:
-    def __init__(self):  # Open file for read/write when class define
-        self.file_path = '/home/mile/Documents/ALL_ip.csv'
-        if os.path.exists(self.file_path):  # Open file or create
-            self.wr_file = open(self.file_path, 'r+', newline='')
+    default_file_path = 'C:/Users/mmikec/Desktop/scripts/ALL_IP.csv'
+    def __init__(self):
+        if os.path.exists(self.default_file_path):  # Open file or create
+            self.wr_file = open(self.default_file_path, 'r+', newline='')
         else:
-            self.wr_file = open(self.file_path, 'w', newline='')
+            self.wr_file = open(self.default_file_path, 'w', newline='')
+
+    def open_file(self, file_path):  # Open file for read/write when class define
+        if os.path.exists(file_path):  # Open file or create
+            wr_file = open(file_path, 'r+', newline='')
+        else:
+            wr_file = open(file_path, 'w', newline='')
+        return wr_file
 
     def input_path(self):
         self.file_path = input('Input path: ')
@@ -17,7 +24,9 @@ class NetworkDiscoveryCore:
             print('File exist!')
         else:
             try:
-                self.wr_file = open(self.file_path, 'w', newline='')
+                self.default_file_path = self.file_path
+                print('Cnange default_file_path to :' + self.default_file_path)
+                self.wr_file = open(self.default_file_path, 'w', newline='')
             except FileNotFoundError:
                 print('Invalid input, Try again')
 
@@ -29,13 +38,14 @@ class NetworkDiscoveryCore:
             self.stop_ip = int(input('Input stop_ip ip: '))
         except ValueError:
             self.start_ip = 1
-            self.stop_ip = 256
+            self.stop_ip = 25
 
     def close_file(self):  # Close file
         self.wr_file.close()
 
     def perform_net_discovery(self):  # Define network discovery
         self.input_ip()
+        self.open_file(self.default_file_path)
         for i in range(self.start_ip, self.stop_ip):
             ip = self.subnet + str(i)
             try:
@@ -46,24 +56,25 @@ class NetworkDiscoveryCore:
             except socket.gaierror:
                 print('Invalid input')
                 break
+        self.close_file()
 
     def write_csv_file(self, ip):  # Write ip in .csv
+        self.open_file(self.default_file_path)
         writer = csv.writer(self.wr_file, delimiter=' ')
         format_ip = self.format_input(ip)
         writer.writerow(format_ip)
         print(format_ip)
+        self.close_file()
 
-############################################
     def format_input(self, input):
         input = str(input)
         format_inp = input.replace('[', '').replace(']', '').replace("'", ''). \
             replace('(', '').replace(')', '').replace(', ,', '-').replace(' ', '').replace('->', ' ')
         return format_inp
-############################################
 
     def read_csv_file(self):  # Define read function
         iplist = []
-        re_file = open(self.file_path, 'r')
+        re_file = open(self.default_file_path, 'r')
         reader = csv.reader(re_file)
         for row in reader:
             iplist += row
